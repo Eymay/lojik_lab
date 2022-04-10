761903798c81ac1d3a89a17ce8e9c822b3467402
Eymen Ünay <eymenunay@outlook.com>
2022-04-10 12:12:22 +0300

    düzeltme

--- modules.v
+++ modules.v
@@ -302,9 +302,9 @@ module BA(input [15:0] A,
 wire [15:0] int_sum1, int_sum2;
 wire int_c, borrow;
 //B - A - A yapılıyor, eğer ilk B - A işleminde borrow varsa diğerine geçmeden önce o çıkarılıyor 
-Sixteen_b_full_adder Sixteen_b_full_adder1(.a(B[15:0]), .b(A[15:0]), .X(1), .s(int_sum1[15:0]), .c_out(int_c));
+Sixteen_b_full_adder Sixteen_b_full_adder1(.a(B[15:0]), .b(A[15:0]), .X(1'b1), .s(int_sum1[15:0]), .c_out(int_c));
 not_gate not1(.i_1(int_c), .o(borrow));
-Sixteen_b_full_adder Sixteen_b_full_adder2(.a(B[15:0]), .b(borrow), .X(1), .s(int_sum2[15:0]), .c_out(c_out));
-Sixteen_b_full_adder Sixteen_b_full_adder3(.a(int_sum2[15:0]), .b(A[15:0]), .X(1), .s(s[15:0]), .c_out(c_out));
+Sixteen_b_full_adder Sixteen_b_full_adder2(.a(B[15:0]), .b(borrow), .X(1'b1), .s(int_sum2[15:0]), .c_out(c_out));
+Sixteen_b_full_adder Sixteen_b_full_adder3(.a(int_sum2[15:0]), .b(A[15:0]), .X(1'b1), .s(s[15:0]), .c_out(c_out));
 
 endmodule
