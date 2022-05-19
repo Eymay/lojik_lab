341e5dfe7abc0d83e25c694984a2cb97e9394275
ituitis20-celika19 <arda.deniz2001@gmail.com>
2022-05-19 18:37:56 +0300

    Update simulation.v

--- hw3/simulation.v
+++ hw3/simulation.v
@@ -38,3 +38,69 @@ module CharDecoder_t();
         
    end
  endmodule
+ 
+ 
+ module CharEncoder_t();
+ 
+    reg [25:0] decodedChar;
+    wire [7:0] char;
+    
+    CharEncoder test(.decodedChar(decodedChar),.char(char));
+    
+    initial begin
+        decodedChar = 26'h0000001; #10;
+        decodedChar = 26'h0000002; #10;
+        decodedChar = 26'h0000004; #10;
+        decodedChar = 26'h0000008; #10;
+        decodedChar = 26'h0000010; #10;
+        decodedChar = 26'h0000020; #10;
+        decodedChar = 26'h0000040; #10;
+        decodedChar = 26'h0000080; #10;
+        decodedChar = 26'h0000100; #10;
+        decodedChar = 26'h0000200; #10;
+        decodedChar = 26'h0000400; #10;
+        decodedChar = 26'h0000800; #10;
+        decodedChar = 26'h0001000; #10;
+        decodedChar = 26'h0002000; #10;
+        decodedChar = 26'h0004000; #10;
+        decodedChar = 26'h0008000; #10;
+        decodedChar = 26'h0010000; #10;
+        decodedChar = 26'h0020000; #10;
+        decodedChar = 26'h0040000; #10;
+        decodedChar = 26'h0080000; #10;
+        decodedChar = 26'h0100000; #10;
+        decodedChar = 26'h0200000; #10;
+        decodedChar = 26'h0400000; #10;
+        decodedChar = 26'h0800000; #10;
+        decodedChar = 26'h1000000; #10;
+        decodedChar = 26'h2000000; #10;
+       
+    $finish;
+    end
+endmodule    
+
+
+module CircularLeftShift_test();
+    reg [25:0] data;
+    reg [4:0] shiftAmount;
+    wire [25:0] out;
+    
+    CircularLeftShift test(.data(data),.shiftAmount(shiftAmount),.out(out));
+    
+    initial begin
+    data = 26'h0000001; shiftAmount = 5'b00001; #10;
+    data = 26'h0000002; shiftAmount = 5'b00010; #10;
+    data = 26'h0000004; shiftAmount = 5'b00011; #10;
+    data = 26'h0000008; shiftAmount = 5'b00100; #10;
+    data = 26'h0000010; shiftAmount = 5'b00101; #10;
+    data = 26'h0000020; shiftAmount = 5'b00110; #10;
+    data = 26'h0000040; shiftAmount = 5'b00111; #10;
+    data = 26'h0000080; shiftAmount = 5'b01000; #10;
+    data = 26'h0000100; shiftAmount = 5'b01001; #10;
+    data = 26'h0000200; shiftAmount = 5'b01010; #10;
+    data = 26'h0000400; shiftAmount = 5'b10000; #10;
+    
+    $finish;
+    end
+endmodule    
+ 
